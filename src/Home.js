2abb5b7161880c204ef206298d3e8a19317c1a7b
Nathanael Sumpter <nsumpter98@gmail.com>
2022-10-17 14:48:12 -0400

    data fetch

--- src/Home.js
+++ src/Home.js
@@ -5,35 +5,7 @@ import BlogList from "./BlogList";
 const Home = () => {
 
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
 
-
-
-    // const handleDelete = (id) => {
-    //     const newBlogs = blogs.filter(blog => blog.id !== id);
-    //     setBlogs(newBlogs);
-    // }
-
-
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then(res => {
-                if(!res.ok) {
-                    throw Error("Could not fetch the data for that resource");
-                }
-                return res.json();
-            }).then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            }).catch(err => {
-                setError(err.message);
-                setIsPending(false);
-            });
-
-    }, []);
 
     return (
         <div className="home">
