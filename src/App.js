1dc0c9e38e0bd3127dddb3567e1f579579deaa82
Nathanael Sumpter <nsumpter98@gmail.com>
2022-10-17 15:20:57 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 
 import Navbar from "./Navbar";
 import Home from "./Home";
+import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <div className="App">
-        <Navbar />
-      <div className="content">
-            <Home />
-      </div>
-    </div>
+    <Router>
+        <div className="App">
+            <Navbar />
+          <div className="content">
+              <Switch>
+                  <Route path='/' element={<Home/>} />
+              </Switch>
+          </div>
+        </div>
+    </Router>
   );
 }
 
