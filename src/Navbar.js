bc1b8f3f1dcec457ebf9267e011308d34fb8eb0b
Nathanael Sumpter <nsumpter98@gmail.com>
2022-10-17 13:38:31 -0400

    initial

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+        <h1>React Blog</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create" style={{
+                color: "white",
+                backgroundColor: "#3554f1",
+                borderRadius: "8px"
+            }}>New Blog</a>
+        </div>
+        </nav>
+    );
+}
+
+export default Navbar;
