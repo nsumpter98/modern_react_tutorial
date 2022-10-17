36883c4c8c9e84fc4475a57520fe51c8cc80d4be
Nathanael Sumpter <nsumpter98@gmail.com>
2022-10-17 15:05:09 -0400

    misc

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-        <h1>React Blog</h1>
+        <h1>NSX Technology</h1>
         <div className="links">
             <a href="/">Home</a>
             <a href="/create" style={{
